f81a242d891483301120688b8188d940680a8f71
Oliver Caceres <oliver_caceres@hotmail.com>
2019-03-10 15:40:58 -0400

    first commit

---
+++ OurLib/Sum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurLib
+{
+    public class Sum
+    {
+        public static int Add(int ValueA, int ValueB) {return ValueA + ValueB;}
+        public static float Add(float ValueA, float ValueB) => ValueA + ValueB; 
+    }
+}
---
+++ ourConsole/MainEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ourConsole
+{
+    using OurLib;
+    class MainEntry
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum.Add(1, 3));
+            Console.WriteLine(Sum.Add(0.45f, 4.56f));
+            Console.ReadKey(true);
+        }
+    }
+}
